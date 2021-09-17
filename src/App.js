8ba9a82f62816e3fd721e16dc1b96f916fe46544
SharkTal <liutao.gao@gmail.com>
2021-09-18 00:14:31 +0300

    -checkout

--- src/App.js
+++ src/App.js
@@ -1,17 +1,23 @@
 import './App.css';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import Header from './component/Header';
-
 import Home from './component/Home';
+import Checkout from './component/Checkout';
+
 function App() {
   return (
     <Router>
       <div className="app">
+        <Header />
         <Switch>
           <Route path="/">
-            <Header />
             <Home />
           </Route>
+
+          <Route path="/checkout">
+            <Checkout />
+          </Route>
+
         </Switch>
 
       </div>
---
+++ src/component/Checkout.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Checkout.css'
+
+const Checkout = () => {
+    return (
+        <div className="checkout">
+            <h1>Checkout</h1>
+        </div>
+    )
+}
+
+export default Checkout
