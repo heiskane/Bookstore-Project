0490fa72e71ac02cc7062dcd7d762fa61a6ba242
heiskane <henipe@gmail.com>
2021-09-18 23:25:40 +0300

    PayPal button

---
+++ src/component/BuyButton.js
@@ -0,0 +1,43 @@
+import React from "react";
+import { PayPalScriptProvider, PayPalButtons } from "@paypal/react-paypal-js";
+
+export default class BuyButton extends React.Component {
+	render() {
+		return (
+			<PayPalScriptProvider options={
+				{ 
+					"client-id": "AVRE2PhqcdO4hh6ak49te0ouOyRq3cdngOheyPvCqS7QQew1XOykcvEM4L9X3DGyWRDnyhGsCZtUg62m",
+					"currency": "EUR"
+				}
+			}>
+				<PayPalButtons
+					style = {{ layout: "horizontal" }}
+					createOrder = {(data, actions) => {
+						return fetch('http://localhost:8000/checkout/paypal/order/create/', {
+							method: 'post',
+							headers: { 'Content-Type': 'application/json' },
+							body: JSON.stringify(
+								{ "book_ids": [8, 9, 1] } // Put book IDs here from shopping cart
+							)
+						}).then(function(res) {
+							return res.json();
+						}).then(function(orderData) {
+							// Maybe deal with this server-side
+							return orderData.result.id;
+						});
+					}}
+
+					onApprove = {(data, actions) => {		
+						return fetch('http://localhost:8000/checkout/paypal/order/' + data.orderID + '/capture/', {
+							method: 'post'
+						}).then(function(res) {
+							return res.json();
+						}).then(function(orderData) {
+							console.log(orderData);
+						});
+					}}
+				/>
+			</PayPalScriptProvider>
+		);
+  }
+}
--- src/component/Home.js
+++ src/component/Home.js
@@ -1,13 +1,13 @@
 import React from 'react'
 import Book from './Book'
 import './Home.css'
+
 const Home = () => {
     return (
         <div className="home">
             <div class="home__container">
                 <img className="home__image" src="https://s2.adlibris.com/images/61500292/promo1280x270.jpg" alt=" " />
             </div>
-
             <div class="home__row">
                 {/*  Six books */}
                 <Book />
