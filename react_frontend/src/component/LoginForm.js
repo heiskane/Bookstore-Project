4b6a141fe6c479c3477ecc1e22195f36e52a65ba
heiskane <henipe@gmail.com>
2021-12-06 16:57:11 +0200

    set login form elevation back to 8

--- react_frontend/src/component/LoginForm.js
+++ react_frontend/src/component/LoginForm.js
@@ -76,7 +76,7 @@ class LoginForm extends React.Component {
       return <Redirect to={this.state.redirectTo} />
     }
     return (
-      <Paper elevation={2}>
+      <Paper elevation={8}>
         <form
           onSubmit={this.handleSubmit}
         >
