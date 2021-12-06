769a97e122299bf364a85cd938e257bb28c5fd2e
heiskane <henipe@gmail.com>
2021-12-06 15:06:49 +0200

    add googple play button when site viewed on mobile

--- react_frontend/src/component/Home.js
+++ react_frontend/src/component/Home.js
@@ -1,11 +1,17 @@
 import React from 'react'
 import Box from '@mui/material/Box'
+import useMediaQuery from '@mui/material/useMediaQuery';
+import MobileStoreButton from 'react-mobile-store-button';
 
 import './Home.css'
 import Books from './Books'
 
 const Home = () => {
+    
+    const isMobile = useMediaQuery('(max-width:600px)');
+    
     return (
+        <>
         <Box sx={{
           display: 'flex',
           flexDirection: 'column',
@@ -15,8 +21,16 @@ const Home = () => {
           marginRight: 'auto',
           maxWidth: '1500px',
         }}>
+            {isMobile &&
+                <MobileStoreButton
+                    store="android"
+                    url="https://play.google.com/store/apps/details?id=com.hlg.books"
+                    linkProps={{ title: "Get it on Google Play" }}
+                />
+            }
             <Books />
         </Box>
+        </>
     )
 }
 
